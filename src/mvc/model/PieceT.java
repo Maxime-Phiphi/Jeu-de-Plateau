295fce727a31d08ff77d5c7b86a2dc8018700026
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2018-02-27 17:57:07 +0100

    piece T

--- src/mvc/model/PieceT.java
+++ src/mvc/model/PieceT.java
@@ -1,6 +1,27 @@
 package mvc.model;
 
-public class PieceT {
+public class PieceT extends Piece{
     public PieceT() {
+        identifiant = 3;
+        tabPiece = new int[][]{
+                {0, 0, 0},
+                {identifiant, identifiant, identifiant},
+                {0, identifiant, 0}
+        };
+    }
+
+    @Override
+    public void rotation() {
+        switch(etat) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
     }
 }
