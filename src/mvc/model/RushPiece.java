4129f5881a67fa62b1bf64dceb8301c6caaec3b1
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2018-03-13 16:51:59 +0100

    RushPiece complete + coordonnées

--- src/mvc/model/RushPiece.java
+++ src/mvc/model/RushPiece.java
@@ -5,9 +5,13 @@ public class RushPiece {
     private String id;
     private String type;
     private String[] tabPiece;
+    private int x;
+    private int y;
 
-    public RushPiece(String type, String id) {
+    public RushPiece(String type, String id, int x, int y) {
         this.id = id;
+        this.x = x;
+        this.y = y;
         if (type=="C"){
             String[] tabPiece = {id, id};
         }
